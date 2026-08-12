3486df59f948a863e9abe81951b3da4af22a177c
jeevi <jeevikadirvel@gmail.com>
2026-08-12 10:48:49 +0530

    Add CI/CD backup automation

---
+++ backup/hello.sh
@@ -0,0 +1,1 @@
+echo "Agile project complete"
---
+++ scripts/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Starting backup task..."
+
+mkdir -p backup
+
+cp hello.sh backup/hello.sh
+
+echo "Backup completed successfully."
+
